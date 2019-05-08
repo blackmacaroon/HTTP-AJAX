bcb4eabecadc1450a41a61f5c19ac41767a2e14e
Kayla <6lackmacaroon@gmail.com>
2019-05-08 14:09:50 -0600

    deconstructed and building everything in app.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import './App.css';
-import FriendsList from './components/friendsList'
+import friendsData from './components/friendsData';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      friends: []
+      friends: friendsData
     };
   }
+
+  FriendsList() {
+    return (
+      <div>
+        {this.state.friends.map(friend => {
+
+        })}
+      </div>
+    )
+  }
+
 
   render() {
     return (
       <div className='App'>
-        <FriendsList />
+        
       </div>
     );
   }
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import friendsData from './friendsData';
+
+class FriendsList extends React.Component {
+
+      
+
+}
+
+
+
+export default FriendsList;
