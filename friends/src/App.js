554a3bf27997e6f7807526fba3c186dea4c836e1
Kayla <6lackmacaroon@gmail.com>
2019-05-09 17:08:53 -0600

    navlinks and routes build on app.js - combined similar forms into one form

--- friends/src/App.js
+++ friends/src/App.js
@@ -8,15 +8,19 @@ import {
 } from 'react-router-dom';
 import axios from 'axios';
 
+import Home from './components/Home';
+import Friend from './components/Friend';
 import FriendList from './components/FriendList';
-import NewFriendForm from './components/NewFriendForm';
-import UpdateFriendForm from './components/UpdateFriendForm';
+import FriendForm from './components/FriendForm';
+
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       activeItem: null,
+      editingFriendId: null,
+      isEditing: false,
       friends: [],
       error: ''
     };
@@ -41,8 +45,8 @@ class App extends React.Component {
 
   changeHandler = e => {
     this.setState({
-      friends: {
-        ...this.state.friends,
+      friend: {
+        ...this.state.friend,
         [e.target.name]: e.target.value
       }
     })
@@ -54,74 +58,103 @@ class App extends React.Component {
       .post('http://localhost:5000/friends', newFriend)
       .then(res => {
         this.setState({ friends: res.data });
+        this.props.history.push('/friend-list');
         // console.log('add friend res', res)
       })
       .catch(err => console.log(err));
   }
   
-  updateFriend = updatedFriend => {
-    axios
-    .put(`http://localhost:5000/friends/${updatedFriend.id}`)
-    .then(res => {
-      this.setState({ friends: res.data})
-      console.log('update friend res', res)
-    .catch(err => console.log(err));
-    })
-  }
+  // updateFriend = updatedFriend => {
+  //   axios
+  //   .put(`http://localhost:5000/friends/${updatedFriend.id}`)
+  //   .then(res => {
+  //     this.setState({ friends: res.data})
+  //     console.log('update friend res', res)
+  //   .catch(err => console.log(err));
+  //   })
+  // }
 
-  deleteFriend = id => {
+  deleteFriend = (e, id) => {
+    e.preventDefault();
     axios
       .delete(`http://localhost:5000/friends/${id}`)
       .then(res => {
-        this.setState({ friends: res.data})
-        console.log('byebye', res)
+        this.setState({ friends: res.data});
+        this.props.history.push('/friends-list');
+        // console.log('byebye', res)
+      })
       .catch(err => console.log(err));
-    })
+  };
+
+  updateFriend = () => {
+    axios
+      .put(
+        `http://localhost:5000/friends/${this.state.editingId}`,
+        this.state.friend
+      )
+      .then(res => {
+        this.setState({
+          friends: res.data,
+          editingId: null,
+          isEditing: false,
+        });
+      })
+      .catch(error => console.log(error));
+  };
+
+  setUpUpdateForm = (e, friend) => {
+    e.preventDefault();
+    this.setState({
+      friend, // same as friend: friend,
+      isEditing: true,
+      editingId: friend.id
+    });
   };
  
   render() {
     return (
       <div className='App'>
-        { this.state.friends.map(friend => 
-          <nav>
-            <h1>Kayla's Best Friends</h1>
-            <div className="nav-links">
-              <NavLink exact to='/'>Home</NavLink>
-              <NavLink to='/friend-form'>Add Friend</NavLink>
-              <NavLink to='/friend-list'>Friends</NavLink>
-    
-              {/* <h1>{friend.name}</h1>
-                <p>{friend.age} years old</p>
-                <p>email: {friend.email}</p>
-                <p>{friend.weight}ish pounds</p>
-                <button onClick={this.updateFriend} className="md-button">
-                  Update Friend
-                </button>
-                <button onClick={this.deleteFriend} className="md-button">
-                  Remove Friend
-                </button> */}
-            </div>
-          </nav>
-          <Route exact path='/' component={Home}/>
-          <Route exact path='/friend-list'
-            render={props => (
-              <FriendList
-                {...props}
-                friends={this.state.friends} />
-            )}
-          >
-          </Route>
-          <Route path='/friend-form'
-            render={props => (
-              <NewFriendForm 
-                {...props}
-                friends={this.state.friends} 
-                addFriend={this.addFriend}
-              />
-            )} 
-          >
-          </Route>
-        )}
+        <nav>
+          <h1>Kayla's Best Friends</h1>
+          <div className="nav-links">
+            <NavLink exact to='/'>Home</NavLink>
+            <NavLink to='/friend-form'>{this.setState.isEditing ? 'Update Friend' : 'Add Friend'}</NavLink>
+            <NavLink to='/friend-list'>Friends</NavLink>
+          </div>
+        </nav>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/friend-list'
+               render={props => (
+                <FriendList
+                  {...props}
+                  friends={this.state.friends}
+                  getItemById={this.getItemById} 
+                />
+               )}
+        >
+        </Route>
+        <Route path='/friend-list/:id'
+               render={props => (
+                <Friend 
+                  {...props}
+                  deleteFriend={this.deleteFriend}
+                  friend={this.state.activeItem}
+                  updateItem={this.setUpUpdateForm}
+            />
+          )} 
+        >
+        </Route>
+        <Route path='/friend-form'
+               render={props => (
+                <FriendForm 
+                  {...props}
+                  activeFriend={this.state.activeItem} 
+                  addFriend={this.addFriend}
+                  updateFriend={this.updateFriend}
+            />
+          )} 
+        >
+        </Route>
       </div>
     );
   }
