5d618ee7fec72f9ccfc858aba7799c5e6f4b5970
Kayla <6lackmacaroon@gmail.com>
2019-05-09 18:07:58 -0600

    friendlist.js built - friend list is list of names - click name to see friend state

---
+++ friends/src/components/Friend.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Route, NavLink } from 'react-router-dom';
+import axios from 'axios';
+
+function Friend ({ friend, deleteFriend, history, updateFriend })
+      // const friend = props.friends.find(
+      //       thing => `${thing.id` === props.match.params.id
+      // );
+      if (!friend) {
+            return <h2>Wait for it....</h2>
+      }
+      return (
+            <div className='friend-wrap'>
+                  <div className='friend-head'>
+                        <h3>{friend.name}</h3>
+                  </div>
+                  <div className='friend-stats'>
+                        <p>{friend.age} years old</p>
+                        <p>email: {friend.email}</p>
+                        <p>{friend.weight}ish pounds</p>
+                  </div>
+                  <button 
+                        onClick={e => {
+                              updateItem(e, friend);
+                              history.push('/friend-form')
+                        }} 
+                        className="md-button">
+                              Update Friend
+                  </button>
+                  <button 
+                        onClick={e => {
+                              deleteFriend(e, friend.id);
+                              history.push('/friend-list');
+                        }} 
+                        className="md-button">
+                              Delete Friend
+                  </button>
+                  
+
+            </div>
+      )
+
+
+export default Friend
--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,60 +1,24 @@
 import React from 'react';
-import { Route, NavLink } from 'react-router-dom';
-import axios from 'axios';
 
-class Friend extends React.Component {
-  state = {
-    friend: null
-  };
-
-  componentDidMount(){
-    axios
-    .get(`http://localhost:5000/itemById/${this.props.match.params.id}`)
-    .then(res => this.setState({ friend: res.data }))
-    .catch(err => console.log(err))
+function FriendList(props) {
+  function routeToFriend(e, friend) {
+    e.preventDefault();
+    props.history.push(`/friend-list/${friend.id}`)
+    props.getItemById(friend.id)
   }
 
-  deleteItem = e => {
-    e.preventDefault();
-    this.props.deleteFriend(this.state.friend.id);
-  };
-
-  updateForm = e => {
-    e.preventDefault();
-    this.props.setUpdateForm(this.state.friend);
-  };
-
-  render(){
-    const { friend } = this.state;
-    if (!this.state.friend) {
-      return <h2>Wait for it....</h2>
-    }
-
-    return (
-      <div className="friend-wrapper">
-        {this.props.map(friend => {
-          <div key={props.friend.name} className='friend'>
-            <h1>{friend.name}</h1>
-            <p>{friend.age} years old</p>
-            <p>email: {friend.email}</p>
-            <p>{friend.weight}ish pounds</p>
-            <button onClick={this.deleteItem} className="md-button">
-              Delete Friend
-            </button>
-            <button onClick={this.updateForm} className="md-button">
-              Update Friend
-            </button>
-          </div>
-        })}
-      </div>
-    )
-  }
-
-
-    
-      
+  return (
+    <div className="friends-wrapper">
+      {props.friends.map(friend => {
+        <div 
+          onCLick={e => routeToFriend(e, friend)}
+          key={friend.id} 
+          className='friend'>
+          <h1>{friend.name}</h1>
+        </div>
+      })}
+    </div>
+  );    
 }
 
-
-
-export default Friend;
+export default FriendList;
